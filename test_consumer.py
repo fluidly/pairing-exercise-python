66e3dd7207e48cb36ca5983c400c55c1b2c06e6f
Marcus Mo <marcus.mo@fluidly.com>
2021-04-07 08:38:02 +0100

    Provide invoices test fixture

--- test_consumer.py
+++ test_consumer.py
@@ -1,4 +1,14 @@
+from csv import DictReader
+
+import pytest
+
 from consumer import consume_invoices
+
+
+@pytest.fixture
+def invoices():
+    with open("invoices.csv") as f:
+        yield list(DictReader(f))
 
 
 def test_empty_list():
