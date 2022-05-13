5f06dce9af32daf842a9ad308d6ac6a546282beb
Thomas Ormezzano <thomas.ormezzano@oaknorth.co.uk>
2022-05-13 14:50:10 +0100

    remove unneeded test

--- test_consumer.py
+++ test_consumer.py
@@ -13,18 +13,3 @@ def invoices():
 
 def test_empty_list():
     consume_invoices([])
-
-
-def test_something_with_fixture(invoices):
-    # this just a dummy test to show what the
-    # invoices fixture does and how to use it
-    assert len(invoices) == 11
-    assert invoices[0] == {
-        "amount": "1000",
-        "due_date": "2019-10-20",
-        "id": "1",
-        "organisation_id": "1000",
-        "paid_date": "2019-10-15",
-        "raised_date": "2019-10-10",
-        "status": "PAID",
-    }
